bf17219f426b61f3c8de1701cf2049b5d41abfb1
Arda Şalvarlılar <ardasalvarlilar@192.168.1.61>
2024-04-30 19:01:00 +0300

    checkpoint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,5 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
-  rules: {
-    // Enforce the use of "type" for type definitions instead of "interface"
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type']
-  }
+  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended']
 }
