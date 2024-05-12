5861f4af305c25d914f46f7f178b9d004f76c47f
Arda Şalvarlılar <ardasalvarlilar@192.168.1.9>
2024-05-12 16:01:05 +0300

    added select box

--- src/app/pages/coach/CoachAddTrainingPage.tsx
+++ src/app/pages/coach/CoachAddTrainingPage.tsx
@@ -4,6 +4,7 @@ import AppLayout from '../../components/layout/AppLayout'
 import InputField from '../../components/ui/InputField'
 import { addTrainingPageTexts } from '../../../utils/constants/texts'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import RNPickerSelect from 'react-native-picker-select';
 
 import { useCreateEventMutation } from '../../../features/query/eventCreateService'
 
@@ -45,14 +46,49 @@ const CoachAddTrainingPage = ({ route }) => {
     // AsyncStorage.setItem('access_token',"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI2NjFiMDBiOWMzNzllZjUxOWRlYWFkMzkiLCJpYXQiOjE3MTQ4NDQ1OTEsIm5iZiI6MTcxNDg0NDU5MSwianRpIjoiZjUzZjg4NWItYmExZi00NzI2LWEwOTMtMGUxNzJhNjRmYzM3IiwiZXhwIjoxNzE0ODQ2MzkxLCJ0eXBlIjoiYWNjZXNzIiwiZnJlc2giOmZhbHNlfQ.KXIXMU15nehfwCiRE0Ju_NMyxgyjAdmNT-sMAwmZeAxy1GO01glA56ilFKIt7uYFZGvM_xWSje26kz30gkXKeFs6JZIymTyO_Qz5v3vMOBozG99sFE0OY3nyz3KKD3HXawDOWOLEBKRRfY-gJ06ompyf_wWigYbNrOJm-ghlaio")
   
   }
+  const eventTypes = [
+    {label: 'Team Training', value: '1'},
+    {label: 'Meeting', value: '2'},
+    {label: 'Weight Lifting', value: '3'},
+  ]
+
+  const trainingLocations = [
+    {label: 'Ege Üni. Büyük S.S.', value: '1'},
+    {label: 'Ege Üni. Büyük S.S. üst kat', value: '2'},
+    {label: 'Ege Üni. 50. Yıl S.S.', value: '3'},
+    {label: 'Ege Üni. Spor Bil. Fak. Top. Sal.', value: '4'},
+    {label: 'Ege Üni. Spor Bil. Fak. Fit. Sal.', value: '5'},
+    {label: 'Bornova Anadolu Lisesi', value: '6'},
+    {label: 'Ege Üni. Atletizm Pisti', value: '7'},
+  ]
 
   return (
     <AppLayout>
       <View className='justify-center flex-1 w-full h-full'>
-        <InputField placeholder={addTrainingPageTexts.trainingTypePlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='event_type' additionalStyles='border-b border-dacka-gray my-3 text-base' />
+
+        <View className='w-full my-3 border-b border-dacka-gray'>
+          <RNPickerSelect
+            darkTheme={true}
+            onValueChange={(value) => console.log(value)}
+            placeholder={{label: 'Select an event type', value: null}}
+            textInputProps={{color:'#919191'}}
+            items={eventTypes}
+          />
+        </View>
+
+        {/* <InputField placeholder={addTrainingPageTexts.trainingTypePlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='event_type' additionalStyles='border-b border-dacka-gray my-3 text-base' /> */}
         <InputField placeholder={addTrainingPageTexts.datePlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='event_date' additionalStyles='border-b border-dacka-gray my-3 text-base' />
         <InputField placeholder={addTrainingPageTexts.timePlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='training_time' additionalStyles='border-b border-dacka-gray my-3 text-base' />
-        <InputField placeholder={addTrainingPageTexts.locationPlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='place' additionalStyles='border-b border-dacka-gray my-3 text-base' />
+        {/* <InputField placeholder={addTrainingPageTexts.locationPlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='place' additionalStyles='border-b border-dacka-gray my-3 text-base' /> */}
+        <View className='w-full my-3 border-b border-dacka-gray'>
+          <RNPickerSelect
+            darkTheme={true}
+            onValueChange={(value) => console.log(value)}
+            placeholder={{label: 'Select a location', value: null}}
+            textInputProps={{color:'#919191'}}
+            items={trainingLocations}
+          />
+        </View>
       </View>
       <View className='flex-row justify-end w-full'>
         <TouchableOpacity className='p-4 rounded-3xl bg-dacka-gray' onPress={postForm}>
