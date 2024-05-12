bd99c3df00a730675167c3963b6ade46243da63f
Arda Şalvarlılar <ardasalvarlilar@192.168.1.9>
2024-05-12 16:55:45 +0300

    added date-picker and components fixed

--- src/app/pages/coach/CoachAddTrainingPage.tsx
+++ src/app/pages/coach/CoachAddTrainingPage.tsx
@@ -1,66 +1,69 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity,ScrollView } from 'react-native'
+import React, { useEffect } from 'react'
 import AppLayout from '../../components/layout/AppLayout'
 import InputField from '../../components/ui/InputField'
 import { addTrainingPageTexts } from '../../../utils/constants/texts'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import RNPickerSelect from 'react-native-picker-select';
-
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { useCreateEventMutation } from '../../../features/query/eventCreateService'
+import { RootState } from '../../../../store';
+import { getAuthUser } from '../../../features/auth/auth.slice';
+import { useSelector } from 'react-redux';
 
 const CoachAddTrainingPage = ({ route }) => {
-  const date = new Date(1714844392303);
-
+  const user = useSelector((state: RootState) => getAuthUser(state));
   const { team_id, attendanceList } = route.params;
+  const dateNow = new Date();
 
   const [trainingForm, setTrainingForm] = React.useState({
-    "event_type": "Game",
-    "creator_id": "string",
-    "place": "string",
-    "event_date": "2024-05-04T17:36:31.336Z",
-    "team_id": "string",
-    "description": "string"
+    "event_type": 1,
+    "creator_id": user?._id,
+    "event_date": dateNow,
+    "team_id": team_id,
+    "event_location": 'string',
   })
 
   const [createEventMutation] = useCreateEventMutation(); // Destructure the createEvent mutation from the API service
 
-  const handleInputChange = (text:string, name:string) => {
+  const handleDateChange = (date:Date | undefined, name:string) => {
     setTrainingForm((prevState) => ({
       ...prevState,
-      [name]: text,
+      [name]: date,
     }));
   }
 
   const postForm = async () => {
-    try {
-      const response = await createEventMutation(trainingForm); // Call the createEvent mutation with the trainingForm data
-      if ('data' in response) {
-        console.log('Event created:', response.data); // Log the response data if successful
-      } else if ('error' in response) {
-        console.error('Error creating event:', response.error); // Log the error if there's an error
-      }
-    } catch (error) {
-      console.error('Error creating event:', error); // Log any unexpected errors
-    }
-    // console.log(trainingForm)
-    // AsyncStorage.setItem('access_token',"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI2NjFiMDBiOWMzNzllZjUxOWRlYWFkMzkiLCJpYXQiOjE3MTQ4NDQ1OTEsIm5iZiI6MTcxNDg0NDU5MSwianRpIjoiZjUzZjg4NWItYmExZi00NzI2LWEwOTMtMGUxNzJhNjRmYzM3IiwiZXhwIjoxNzE0ODQ2MzkxLCJ0eXBlIjoiYWNjZXNzIiwiZnJlc2giOmZhbHNlfQ.KXIXMU15nehfwCiRE0Ju_NMyxgyjAdmNT-sMAwmZeAxy1GO01glA56ilFKIt7uYFZGvM_xWSje26kz30gkXKeFs6JZIymTyO_Qz5v3vMOBozG99sFE0OY3nyz3KKD3HXawDOWOLEBKRRfY-gJ06ompyf_wWigYbNrOJm-ghlaio")
-  
+    // try {
+    //   const response = await createEventMutation(trainingForm); // Call the createEvent mutation with the trainingForm data
+    //   if ('data' in response) {
+    //     console.log('Event created:', response.data); // Log the response data if successful
+    //   } else if ('error' in response) {
+    //     console.error('Error creating event:', response.error); // Log the error if there's an error
+    //   }
+    // } catch (error) {
+    //   console.error('Error creating event:', error); // Log any unexpected errors
+    // }
+    console.log(trainingForm) 
   }
   const eventTypes = [
-    {label: 'Team Training', value: '1'},
-    {label: 'Meeting', value: '2'},
-    {label: 'Weight Lifting', value: '3'},
+    {label: 'Team Training', value: 1},
+    {label: 'Meeting', value: 2},
+    {label: 'Weight Lifting', value: 3},
   ]
 
   const trainingLocations = [
-    {label: 'Ege Üni. Büyük S.S.', value: '1'},
-    {label: 'Ege Üni. Büyük S.S. üst kat', value: '2'},
-    {label: 'Ege Üni. 50. Yıl S.S.', value: '3'},
-    {label: 'Ege Üni. Spor Bil. Fak. Top. Sal.', value: '4'},
-    {label: 'Ege Üni. Spor Bil. Fak. Fit. Sal.', value: '5'},
-    {label: 'Bornova Anadolu Lisesi', value: '6'},
-    {label: 'Ege Üni. Atletizm Pisti', value: '7'},
+    {label: 'Ege Üni. Büyük S.S.', value: 1},
+    {label: 'Ege Üni. Büyük S.S. üst kat', value: 2},
+    {label: 'Ege Üni. 50. Yıl S.S.', value: 3},
+    {label: 'Ege Üni. Spor Bil. Fak. Top. Sal.', value: 4},
+    {label: 'Ege Üni. Spor Bil. Fak. Fit. Sal.', value: 5},
+    {label: 'Bornova Anadolu Lisesi', value: 6},
+    {label: 'Ege Üni. Atletizm Pisti', value: 7},
   ]
+
+  const [showDate, setShowDate] = React.useState(false);
+  const [showTime, setShowTime] = React.useState(false);
 
   return (
     <AppLayout>
@@ -69,21 +72,28 @@ const CoachAddTrainingPage = ({ route }) => {
         <View className='w-full my-3 border-b border-dacka-gray'>
           <RNPickerSelect
             darkTheme={true}
-            onValueChange={(value) => console.log(value)}
+            onValueChange={(value) => setTrainingForm((prevState) => ({...prevState, event_type: value}))}
             placeholder={{label: 'Select an event type', value: null}}
             textInputProps={{color:'#919191'}}
             items={eventTypes}
           />
         </View>
 
-        {/* <InputField placeholder={addTrainingPageTexts.trainingTypePlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='event_type' additionalStyles='border-b border-dacka-gray my-3 text-base' /> */}
-        <InputField placeholder={addTrainingPageTexts.datePlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='event_date' additionalStyles='border-b border-dacka-gray my-3 text-base' />
-        <InputField placeholder={addTrainingPageTexts.timePlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='training_time' additionalStyles='border-b border-dacka-gray my-3 text-base' />
-        {/* <InputField placeholder={addTrainingPageTexts.locationPlaceholder} placeholderTextColor='light' keyboardType="default" handleInputChange={handleInputChange} name='place' additionalStyles='border-b border-dacka-gray my-3 text-base' /> */}
+
+
+        <TouchableOpacity className='my-3 border-b border-dacka-gray' onPress={() => setShowDate((prevState) => !prevState)}>
+          <Text className='text-dacka-gray'>Add Date</Text> 
+        </TouchableOpacity>
+        {showDate && (
+          <View className='w-full my-3 border-b border-dacka-gray'>
+            <DateTimePicker value={trainingForm.event_date} textColor='#fff' display='spinner' mode='datetime' onChange={(event,date) => handleDateChange(date,'event_date')} />
+          </View>
+        )}
+
         <View className='w-full my-3 border-b border-dacka-gray'>
           <RNPickerSelect
             darkTheme={true}
-            onValueChange={(value) => console.log(value)}
+            onValueChange={(value) => setTrainingForm((prevState) => ({...prevState, event_location: value}))}
             placeholder={{label: 'Select a location', value: null}}
             textInputProps={{color:'#919191'}}
             items={trainingLocations}
@@ -91,7 +101,7 @@ const CoachAddTrainingPage = ({ route }) => {
         </View>
       </View>
       <View className='flex-row justify-end w-full'>
-        <TouchableOpacity className='p-4 rounded-3xl bg-dacka-gray' onPress={postForm}>
+        <TouchableOpacity className='p-4 rounded-3xl bg-dacka-dark-gray' onPress={postForm}>
           <Text className='text-white'>{addTrainingPageTexts.addTraining}</Text>
         </TouchableOpacity>
       </View>
