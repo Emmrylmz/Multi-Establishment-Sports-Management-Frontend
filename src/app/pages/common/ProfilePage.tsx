15d8ab582f149fa472c8a18475fb377e9088df93
Arda Şalvarlılar <ardasalvarlilar@192.168.1.10>
2024-07-05 15:37:53 +0300

    player profile indicator added

--- src/app/pages/common/ProfilePage.tsx
+++ src/app/pages/common/ProfilePage.tsx
@@ -4,28 +4,37 @@ import type { RootState } from '../../../../store';
 import { getAuthUser } from '../../../features/auth/auth.slice';
 import ProfileContainer from '../../components/profile/ProfileContainer';
 import { useGetUserInfoQuery } from '../../../features/query/userInfoQueryService';
-import { Text, View } from 'react-native';
+import { Text, View, ActivityIndicator } from 'react-native';
 import { ProfileHeader } from '../../components';
 
+const LoadingSpinner = () => {
+    return (
+        <View className="items-center justify-center flex-1">
+            <ActivityIndicator size="large" color="#919191" />
+            <Text className="mt-2">Loading...</Text>
+        </View>
+    );
+};
+
 const ProfilePage: React.FC = () => {
-	const user = useSelector((state: RootState) => getAuthUser(state));
-	const { data: UserInfo, isLoading } = useGetUserInfoQuery(user?._id);
-	console.log(UserInfo)
+    const user = useSelector((state: RootState) => getAuthUser(state));
+    const { data: UserInfo, isLoading } = useGetUserInfoQuery(user?._id);
+    console.log(UserInfo)
 
-	if (isLoading) {
-		return <Text>Loading...</Text>;
-	}
+    if (isLoading) {
+        return <LoadingSpinner />;
+    }
 
-	if (!UserInfo) {
-		return <Text>No user information available</Text>;
-	}
+    if (!UserInfo) {
+        return <Text>No user information available</Text>;
+    }
 
-	return (
-		<>
-			<ProfileContainer user={UserInfo} />
-			{/* <ProfileHeader isProfilePage={true} /> */}
-		</>
-	);
+    return (
+        <>
+            <ProfileContainer user={UserInfo} />
+            {/* <ProfileHeader isProfilePage={true} /> */}
+        </>
+    );
 };
 
 export default ProfilePage;
