5832f5aed2a17b2b5e6214d7cea49b6489459ef9
Arda Şalvarlılar <ardasalvarlilar@192.168.1.9>
2024-07-22 19:36:30 +0300

    pushing main

--- src/features/query/constantsQueryService.ts
+++ src/features/query/constantsQueryService.ts
@@ -1,18 +1,39 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { baseQueryWithReauth } from './baseQueryWithReauth';
 
-const constantsQueryService = createApi({
-	reducerPath: 'constantsQueryService',
-	baseQuery: baseQueryWithReauth,
-	endpoints: (builder) => ({
-		getConstantFromKey: builder.query({
-			query: (key) => ({
-				url: `/constants/get/key/${key}`,
-				method: 'GET',
-			}),
-		}),
-	}),
+const ConstantsQueryService = createApi({
+  reducerPath: 'constantsQueryService',
+  baseQuery: baseQueryWithReauth,
+  endpoints: (builder) => ({
+    getConstants: builder.query({
+      query: () => ({
+        url: '/constants/all',
+        method: 'GET',
+      }),
+    }),
+    createConstant: builder.mutation({
+      query: (constant) => ({
+        url: '/constants/create',
+        method: 'POST',
+        body: constant,
+      }),
+    }),
+    updateConstant: builder.mutation({
+      query: (constant) => ({
+        url: `/constants/update/${constant.id}`,
+        method: 'PUT',
+        body: constant,
+      }),
+    }),
+    deleteConstant: builder.mutation({
+      query: (constant) => ({
+        url: `/constants/delete/${constant.id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
 });
 
-export const { useGetConstantFromKeyQuery } = constantsQueryService;
-export default constantsQueryService;
+export const { useGetConstantsQuery, useCreateConstantMutation, useUpdateConstantMutation, useDeleteConstantMutation } = ConstantsQueryService;
+export default ConstantsQueryService;
