a7c0b6698648e0ce26c21df0d9d5626f3fc781da
Emmrylmz <88165258+Emmrylmz@users.noreply.github.com>
2024-07-23 16:53:42 +0300

    tab bar for manager payments page

--- src/navigation/Manager/ManagerPaymentStackNavigator.tsx
+++ src/navigation/Manager/ManagerPaymentStackNavigator.tsx
@@ -4,6 +4,7 @@ import ManagerHomePage from '../../app/pages/manager/ManagerHomePage';
 import { ManagerPaymentStack } from '../StackNavigators';
 import AddUserPage from '../../app/pages/coach/AddUserPage';
 import ManagerPaymentPage from '../../app/pages/manager/ManagerPaymentPage';
+import TopTabs from './ManagerPaymentTopNavigator';
 
 const ManagerPaymentStackNavigator = () => (
 	<ManagerPaymentStack.Navigator screenOptions={{ headerShown: false }}>
@@ -11,6 +12,7 @@ const ManagerPaymentStackNavigator = () => (
 			name="ManagerPaymentPage"
 			component={ManagerPaymentPage}
 		/>
+		<ManagerPaymentStack.Screen  name="PlayerPayments" component={TopTabs} />
 		{/* Add manager-specific screens here */}
 	</ManagerPaymentStack.Navigator>
 );
